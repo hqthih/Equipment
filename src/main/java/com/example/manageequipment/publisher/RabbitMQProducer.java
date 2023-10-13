66e2ef4d654f29cda9be80aac6dc0aaded37c554
hqdat809 <dathq@eupgroup.net>
2023-10-13 23:48:37 +0700

    a

--- src/main/java/com/example/manageequipment/publisher/RabbitMQProducer.java
+++ src/main/java/com/example/manageequipment/publisher/RabbitMQProducer.java
@@ -1,61 +1,18 @@
 package com.example.manageequipment.publisher;
 
-import com.example.manageequipment.config.RabbitMQConfig;
-import com.example.manageequipment.dto.NotificationDto;
 import com.example.manageequipment.dto.RequestDto;
-import com.example.manageequipment.model.Role;
-import com.example.manageequipment.model.User;
-import com.example.manageequipment.repository.RoleCustomRepo;
-import com.example.manageequipment.repository.UserRepository;
-import com.example.manageequipment.service.FCMService;
-import com.example.manageequipment.service.NotificationService;
-import com.example.manageequipment.service.RequestService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.firebase.messaging.FirebaseMessagingException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
-import java.util.List;
 
 @Service
 public class RabbitMQProducer {
-    @Value("${rabbit.exchange.manage_equipment}")
-    private String exchange;
-
-    @Value("${rabbit.routing.push_notification}")
-    private String push_notification_routingKey;
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
-
-    @Autowired
-    private RequestService requestService;
-
-    @Autowired
-    private RabbitMQConfig rabbitMQConfig;
-
-    @Autowired
-    private RoleCustomRepo roleCustomRepo;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private NotificationService notificationService;
-
-    @Autowired
-    private FCMService fcmService;
 
     public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
